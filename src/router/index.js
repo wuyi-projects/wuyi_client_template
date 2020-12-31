bab2f7a3d69fdb1f827f8c97cdd85ed1d216a94d
WuYiTechnology <ziqiang-hpu@qq.com>
2020-12-31 10:08:15 +0800

    新增发票路由、页面

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,7 @@ import taskRouter from './modules/task'
 import newsRouter from './modules/news'
 import configRouter from './modules/config'
 import fileRouter from './modules/file'
+import fapiaoRouter from './modules/fapiao'
 
 /**
  * constantRoutes
@@ -21,64 +22,65 @@ import fileRouter from './modules/file'
  * all roles can be accessed
  */
 export const constantRoutes = [{
-        path: '/redirect',
-        component: Layout,
-        hidden: true,
-        children: [{
-            path: '/redirect/:path(.*)',
-            component: () =>
-                import ('@/views/redirect/index')
-        }]
-    },
-    {
-        path: '/login',
-        component: () =>
-            import ('@/views/login/index'),
-        hidden: true
-    },
+  path: '/redirect',
+  component: Layout,
+  hidden: true,
+  children: [{
+    path: '/redirect/:path(.*)',
+    component: () =>
+      import ('@/views/redirect/index')
+  }]
+},
+{
+  path: '/login',
+  component: () =>
+    import ('@/views/login/index'),
+  hidden: true
+},
 
-    {
-        path: '/404',
-        component: () =>
-            import ('@/views/error-page/404'),
-        hidden: true
-    },
-    {
-        path: '/',
-        component: Layout,
-        redirect: '/dashboard',
-        children: [{
-            path: 'dashboard',
-            name: 'Dashboard',
-            component: () =>
-                import ('@/views/dashboard/index'),
-            meta: { title: '首页', icon: 'dashboard' }
-        }]
-    }
+{
+  path: '/404',
+  component: () =>
+    import ('@/views/error-page/404'),
+  hidden: true
+},
+{
+  path: '/',
+  component: Layout,
+  redirect: '/dashboard',
+  children: [{
+    path: 'dashboard',
+    name: 'Dashboard',
+    component: () =>
+      import ('@/views/dashboard/index'),
+    meta: { title: '首页', icon: 'dashboard' }
+  }]
+}
 ]
 
 export const asyncRoutes = [
-    taskRouter,
-    newsRouter,
-    oaRouter,
-    userRouter,
-    configRouter,
-    fileRouter,
-    permissionRouter,
-    { path: '*', redirect: '/404', hidden: true }
+  taskRouter,
+  newsRouter,
+  oaRouter,
+  userRouter,
+  configRouter,
+  fileRouter,
+  fapiaoRouter,
+  permissionRouter,
+  { path: '*', redirect: '/404', hidden: true }
 ]
 
 const createRouter = () =>
-    new Router({
-        scrollBehavior: () => ({ y: 0 }),
-        routes: constantRoutes
-    })
+  new Router({
+    scrollBehavior: () => ({ y: 0 }),
+    routes: constantRoutes
+  })
 
 const router = createRouter()
 
 export function resetRouter() {
-    const newRouter = createRouter()
-    router.matcher = newRouter.matcher
+  const newRouter = createRouter()
+  router.matcher = newRouter.matcher
 }
 
 export default router
