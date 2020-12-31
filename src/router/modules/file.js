d4aff75f281f080caff2387ce55c5e289ea36384
WuYiTechnology <ziqiang-hpu@qq.com>
2020-12-31 09:09:12 +0800

    移除废弃代码

--- src/router/modules/file.js
+++ src/router/modules/file.js
@@ -1,25 +1,19 @@
 import Layout from '@/layout'
 
 const fileRouter = {
-    path: '/file',
-    component: Layout,
-    meta: {
-        title: '文件管理',
-        icon: 'file',
-        roles: ['PermissionAdministrator', 'BusinessAdmistrator']
-    },
-    children: [{
-        path: 'file-management',
-        component: () =>
-            import ('@/views/file/file-management'),
-        name: 'file-management',
-        meta: { title: '文件管理', roles: ['PermissionAdministrator'] }
-    }, {
-        path: 'file-management2',
-        component: () =>
-            import ('@/views/file/file-management2'),
-        name: 'file-management2',
-        meta: { title: '文件管理2', roles: ['PermissionAdministrator'] }
-    }, ]
+  path: '/file',
+  component: Layout,
+  meta: {
+    title: '文件管理',
+    icon: 'file',
+    roles: ['PermissionAdministrator', 'BusinessAdmistrator']
+  },
+  children: [{
+    path: 'file-management',
+    component: () =>
+      import ('@/views/file/file-management'),
+    name: 'file-management',
+    meta: { title: '文件管理', roles: ['PermissionAdministrator'] }
+  }]
 }
 export default fileRouter
