57ca976e9ba29ca9729dd89910b3d37744f28dac
yang123yyy <1611259620@qq.com>
2021-01-26 22:27:32 +0800

    图标样式变换

--- src/views/table/table.vue
+++ src/views/table/table.vue
@@ -1,28 +1,38 @@
 <template>
   <div class="app-container">
     <el-card class="box-card">
-      <el-row>
+      <el-row :gutter="20">
         <el-col :span="12">
-          <div>
-            <el-card class="box-card">
-              <div id="roomTypeCheckIn" />
-            </el-card>
-          </div>
+          <el-card>
+            <div slot="header" class="clearfix" style="text-align:center;">
+              <span>卡片名称</span>
+            </div>
+            <div id="roomTypeCheckIn" />
+          </el-card>
         </el-col>
         <el-col :span="12">
-          <el-card class="box-card">
+          <el-card>
+            <div slot="header" class="clearfix" style="text-align:center;">
+              <span>卡片名称</span>
+            </div>
             <div id="news" />
           </el-card>
         </el-col>
       </el-row>
-      <el-row>
+      <el-row style="margin-top:20px" :gutter="20">
         <el-col :span="12">
           <el-card class="box-card">
+            <div slot="header" class="clearfix" style="text-align:center;">
+              <span>卡片名称</span>
+            </div>
             <div id="lines" />
           </el-card>
         </el-col>
         <el-col :span="12">
           <el-card class="box-card">
+            <div slot="header" class="clearfix" style="text-align:center;">
+              <span>卡片名称</span>
+            </div>
             <div id="histtogram" />
           </el-card>
         </el-col>
@@ -30,7 +40,11 @@
     </el-card>
   </div>
 </template>
-
+<style>
+.el-card__header {
+  border-bottom: none;
+}
+</style>
 <script>
 import { Chart } from '@antv/g2'
 
@@ -265,74 +279,70 @@ export default {
       interval.elements[0].setState('selected', true)
 
       // 折线图
-      const lineData = [
-        { month: 'Jan', city: 'Tokyo', temperature: 7 },
-        { month: 'Jan', city: 'London', temperature: 3.9 },
-        { month: 'Feb', city: 'Tokyo', temperature: 6.9 },
-        { month: 'Feb', city: 'London', temperature: 4.2 },
-        { month: 'Mar', city: 'Tokyo', temperature: 9.5 },
-        { month: 'Mar', city: 'London', temperature: 5.7 },
-        { month: 'Apr', city: 'Tokyo', temperature: 14.5 },
-        { month: 'Apr', city: 'London', temperature: 8.5 },
-        { month: 'May', city: 'Tokyo', temperature: 18.4 },
-        { month: 'May', city: 'London', temperature: 11.9 },
-        { month: 'Jun', city: 'Tokyo', temperature: 21.5 },
-        { month: 'Jun', city: 'London', temperature: 15.2 },
-        { month: 'Jul', city: 'Tokyo', temperature: 25.2 },
-        { month: 'Jul', city: 'London', temperature: 17 },
-        { month: 'Aug', city: 'Tokyo', temperature: 26.5 },
-        { month: 'Aug', city: 'London', temperature: 16.6 },
-        { month: 'Sep', city: 'Tokyo', temperature: 23.3 },
-        { month: 'Sep', city: 'London', temperature: 14.2 },
-        { month: 'Oct', city: 'Tokyo', temperature: 18.3 },
-        { month: 'Oct', city: 'London', temperature: 10.3 },
-        { month: 'Nov', city: 'Tokyo', temperature: 13.9 },
-        { month: 'Nov', city: 'London', temperature: 6.6 },
-        { month: 'Dec', city: 'Tokyo', temperature: 9.6 },
-        { month: 'Dec', city: 'London', temperature: 4.8 }
+      const linesdata = [
+        { country: 'Asia', year: '1750', value: 502 },
+        { country: 'Asia', year: '1800', value: 635 },
+        { country: 'Asia', year: '1850', value: 809 },
+        { country: 'Asia', year: '1900', value: 5268 },
+        { country: 'Asia', year: '1950', value: 4400 },
+        { country: 'Asia', year: '1999', value: 3634 },
+        { country: 'Asia', year: '2050', value: 947 },
+        { country: 'Africa', year: '1750', value: 106 },
+        { country: 'Africa', year: '1800', value: 107 },
+        { country: 'Africa', year: '1850', value: 111 },
+        { country: 'Africa', year: '1900', value: 1766 },
+        { country: 'Africa', year: '1950', value: 221 },
+        { country: 'Africa', year: '1999', value: 767 },
+        { country: 'Africa', year: '2050', value: 133 },
+        { country: 'Europe', year: '1750', value: 163 },
+        { country: 'Europe', year: '1800', value: 203 },
+        { country: 'Europe', year: '1850', value: 276 },
+        { country: 'Europe', year: '1900', value: 628 },
+        { country: 'Europe', year: '1950', value: 547 },
+        { country: 'Europe', year: '1999', value: 729 },
+        { country: 'Europe', year: '2050', value: 408 },
+        { country: 'Oceania', year: '1750', value: 200 },
+        { country: 'Oceania', year: '1800', value: 200 },
+        { country: 'Oceania', year: '1850', value: 200 },
+        { country: 'Oceania', year: '1900', value: 460 },
+        { country: 'Oceania', year: '1950', value: 230 },
+        { country: 'Oceania', year: '1999', value: 300 },
+        { country: 'Oceania', year: '2050', value: 300 }
       ]
-
-      const line = new Chart({
+      const lines = new Chart({
         container: 'lines',
         autoFit: true,
         height: 500
       })
 
-      line.data(lineData)
-      line.scale({
-        month: {
-          range: [0, 1]
-        },
-        temperature: {
-          nice: true
-        }
+      lines.data(linesdata)
+      lines.scale('year', {
+        type: 'linear',
+        tickInterval: 50
+      })
+      lines.scale('value', {
+        nice: true
       })
 
-      line.tooltip({
+      lines.tooltip({
         showCrosshairs: true,
         shared: true
       })
 
-      line.axis('temperature', {
-        label: {
-          formatter: val => {
-            return val + ' °C'
-          }
-        }
-      })
-
-      line
+      lines
+        .area()
+        .adjust('stack')
+        .position('year*value')
+        .color('country')
+      lines
         .line()
-        .position('month*temperature')
-        .color('city')
-        .shape('smooth')
+        .adjust('stack')
+        .position('year*value')
+        .color('country')
 
-      line
-        .point()
-        .position('month*temperature')
-        .color('city')
-        .shape('circle')
-      line.render()
+      lines.interaction('element-highlight')
+
+      lines.render()
     }
   }
 }
